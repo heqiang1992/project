83b601b4132e427fc34a4f08cdd738d3ce7de949
heqiang1992 <651828863@qq.com>
2019-05-05 17:51:01 +0800

    2019.05.05 pytest

---
+++ he/test_pytest.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import pytest
+
+
+def func(x):
+    return x + 1
+
+
+def test_func1():
+    assert func(4) == 5  # 成功示例
+
+
+def test_func2():
+    assert func(3) == 5  # 失败示例
+
+def test_func3():
+    assert True
+
+
+if __name__ == "__main__":
+    pytest.main(["--html=report.html","D:\\code\\pj\\he\\"])
+
+
+# pytest.main()  # 遍历相同目录下的所以test开头的用例
+# pytest.main("-q test_main.py")  # 指定测试文件
+# pytest.main("/root/Documents/python3_1000/1000/python3_pytest")  # 指定测试目录
+# --junitxml=results.xml 成xml文件结果 ，--resultlog=report\log.txt ，--html=report\test_one_func.html
